8982fbdfc2e65e54bc5cc476b4daa5a518568475
Jose Capelan <j.capelan@case-inc.com>
2013-11-20 00:36:30 -0200

    added test and change layout

---
+++ src/module/Application/src/Test/Controller/IndexControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Application\Test\Controller;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class IndexControllerTest extends AbstractHttpControllerTestCase{
+
+    public function setUp()
+    {
+
+
+
+
+        $this->setUseConsoleRequest(false);
+        $this->setApplicationConfig(
+            include getcwd().'/config/application.config.php'
+        );
+        $this->getApplication();
+
+
+
+
+
+    }
+
+    public function testIndexActionCanBeAccessed()
+    {
+        $this->dispatch('/');
+        $this->assertResponseStatusCode(200);
+        $this->assertMatchedRouteName('home');
+        $this->assertControllerName('application\controller\index');
+        $this->assertQuery('.jumbotron','div');
+
+    }
+
+} 
