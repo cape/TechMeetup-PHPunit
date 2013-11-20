601db3fa8968143112d55b7abc7c02fce38a58ab
Jose Capelan <j.capelan@case-inc.com>
2013-11-20 01:38:42 -0200

    added data provider test

---
+++ src/module/Application/src/Test/Provider/Data/FileProviderTest.php
@@ -0,0 +1,20 @@
+<?php
+use Application\Data\Provider\File as DataProvider;
+
+
+class FileProviderTest extends PHPUnit_Framework_TestCase{
+
+    public function setUp(){
+        Zend\Mvc\Application::init(include 'config/application.config.php');
+    }
+    public function testConfigureProvider(){
+
+        $provider = new DataProvider();
+        $provider->configure(array('filename'=> getcwd().'/data/content/list'));
+
+        $this->assertTrue($provider->isValid(),'Data Provider Is not properly configured');
+
+
+    }
+}
+ 
